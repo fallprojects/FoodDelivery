e8c0ccce3ad29f470ab3834724a043ad34debc78
baistan <almazbekovbaistan@gmail.com>
2020-11-09 19:42:12 +0600

    create models

--- OnlineDelivery/models.py
+++ OnlineDelivery/models.py
@@ -1,3 +1,36 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Products(models.Model):
+    CATEGORY = (
+        ('Кальян','Кальян'),
+        ('Табак','Табак'),
+        ('Чаша','Чаша'),
+        ('Угли','Угли'),
+    )
+    name = models.CharField(max_length=50,null=True)
+    price = models.FloatField(null=True)
+    category = models.CharField(max_length=50,null=True,choices=CATEGORY)
+    decsription = models.CharField(max_length=50,null=True,blank=True)
+    date_created = models.DateTimeField(auto_now_add=True,null=True)
+
+
+
+    def __str__(self):
+        return self.name
+
+
+class Adresses(models.Model):
+    adress = models.CharField(max_length=200, null=True)
+    phone = models.CharField(max_length=200, null=True)
+
+
+class Actions(models.Model):
+    first = models.CharField(max_length=200, null=True)
+    second = models.CharField(max_length=200, null=True)
+    third = models.CharField(max_length=200, null=True)
+
+
+class Comments(models.Model):
+    pass
