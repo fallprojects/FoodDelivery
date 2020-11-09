419484e28475cf454fee85b60c13d1437d2a7423
baistan <almazbekovbaistan@gmail.com>
2020-11-09 19:43:48 +0600

    added path of sites

---
+++ OnlineDelivery/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from .views import *
+
+
+urlpatterns = [
+    path('',home_page,name='home'),
+    path('products',products_page,name='products'),
+    path('actions',action_page,name='actions'),
+    path('adresses',contact_page,name='contacts'),
+    path('comments',comment_page,name='comments'),
+]
