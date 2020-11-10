deda018ea9bc5af017f38e92a027e81d68186ae5
baistan <almazbekovbaistan@gmail.com>
2020-11-10 19:34:24 +0600

    add order table

--- OnlineDelivery/admin.py
+++ OnlineDelivery/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from .models import *
 
 
-admin.site.register([Products,Adresses,Actions,Comments])
+admin.site.register([Products,Adresses,Actions,Comments,Orders])
 
