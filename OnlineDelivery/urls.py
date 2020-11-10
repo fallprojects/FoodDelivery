ce3d09f29c793dbfca632310846c2eea2ef7c8fe
baistan <almazbekovbaistan@gmail.com>
2020-11-10 19:38:04 +0600

    add new path "purchase"

--- OnlineDelivery/urls.py
+++ OnlineDelivery/urls.py
@@ -5,8 +5,9 @@ from .views import *
 
 urlpatterns = [
     path('',home_page,name='home'),
-    path('products',products_page,name='products'),
-    path('actions',action_page,name='actions'),
-    path('adresses',contact_page,name='contacts'),
-    path('comments',comment_page,name='comments'),
+    path('products/',products_page,name='products'),
+    path('actions/',action_page,name='actions'),
+    path('adresses/',contact_page,name='contacts'),
+    path('comments/',comment_page,name='comments'),
+    path('purchase/<int:pk>/',purchase,name='purchase')
 ]
